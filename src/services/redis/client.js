f363468c664230cea7e27cea831a7eabb892271c
Jonatan Frank <jonafrank@gmail.com>
2019-03-20 16:15:36 -0300

    fix redis call

--- src/services/redis/client.js
+++ src/services/redis/client.js
@@ -2,9 +2,6 @@ import redis from 'redis';
 import config from '../../config';
 
 console.log('***REDIS CONFIGURATION****', config.redis);
-const client = redis.createClient({
-    host: config.redis.host,
-    port: config.redis.port,
-});
+const client = redis.createClient(`redis://${config.redis.server}:${config.redis.port}`);
 
 export default client;
