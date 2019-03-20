1c90a83bcf7b85f2d3657a18d15213f975ab6b0e
Jonatan Frank <jonafrank@gmail.com>
2019-03-20 16:01:26 -0300

    add debug message

--- src/services/redis/client.js
+++ src/services/redis/client.js
@@ -1,6 +1,7 @@
 import redis from 'redis';
 import config from '../../config';
 
+console.log('***REDIS CONFIGURATION****', config.redis);
 const client = redis.createClient({
     host: config.redis.host,
     port: config.redis.port,
